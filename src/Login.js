bb23690fcef3f18f7bb57edea43351c57b20dbcd
PR <ranipriyanka713@gmail.com>
2017-11-16 19:23:22 +0530

    basic login/reg ui

---
+++ src/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+
+class Login extends React.Component {
+constructor(props){
+  super(props);
+
+  this.state={
+      username: '',
+      password: ''
+    }
+ }
+
+handleLogin(event){
+  console.log("-- handleLogin --");
+}
+
+render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleLogin(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+ margin: 15,
+};
+export default Login;
